9f745263c1ec870f83eb7e607c635971303e6f9a
dolphin1135 <108594205+dolphin1135@users.noreply.github.com>
2025-05-04 00:00:28 +1000

    added Deck Screen

---
+++ src/main/java/git7s/flashcardai/CardDeckController.java
@@ -0,0 +1,25 @@
+package git7s.flashcardai;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+public class CardDeckController {
+    public Label progressTextLabel;
+    @FXML private ProgressBar studyProgressBar;
+    @FXML private Label strongestTopicLabel;
+    @FXML private Label weakestTopicLabel;
+
+    @FXML private Button subjectsButton;
+    @FXML private Button settingsButton;
+    @FXML private Button logOutButton;
+
+    @FXML
+    public void initialize() {
+        // Set placeholder data
+        studyProgressBar.setProgress(0.75); // 75% completion
+        strongestTopicLabel.setText("Programming");
+        weakestTopicLabel.setText("Science");
+    }
+}
