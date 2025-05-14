20e62a8d91e8d6ee78dd823f95db89c53a49d4dc
SamDHarrison <samlord025@gmail.com>
2025-05-14 16:36:32 +1000

    Update Maven surefire plugin

--- src/main/java/git7s/flashcardai/MySubjectsController.java
+++ src/main/java/git7s/flashcardai/MySubjectsController.java
@@ -2,7 +2,6 @@ package git7s.flashcardai;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
