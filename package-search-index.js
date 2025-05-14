c64b5fca05674a8711baee592860bbb4f64b03c9
SamDHarrison <samlord025@gmail.com>
2025-05-14 08:23:33 +0000

    Deploying to gh-pages from @ SamDHarrison/Git7sProject@cac67970ce3f837a921fb5251049a6e5dce9e86a 🚀

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"git7sproject","l":"git7s.flashcardai"},{"m":"git7sproject","l":"git7s.flashcardai.dao"}];updateSearchResults();
